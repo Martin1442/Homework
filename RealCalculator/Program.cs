7d82b91499f03ca1df41775731164e4d16e8639c
Martin1442 <Martin1442@users.noreply.github.com>
2019-02-27 15:56:30 +0100

    RealCalculator
    Homework

---
+++ RealCalculator/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("To start the calculator press Enter");
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter your first number:");
+            string firstNumber = Console.ReadLine();
+
+            Console.WriteLine("Please enter your arithmetic operation sign e.g(+,-,*,/)");
+            string operationSign = Console.ReadLine();
+
+            Console.WriteLine("Please enter your second number:");
+            string secondNumber = Console.ReadLine();
+
+            int firstNum;
+            int secondNum;
+
+            int result = 0;
+
+            bool convertFirstNum = int.TryParse(firstNumber, out firstNum);
+            bool convertSecondNum = int.TryParse(secondNumber, out secondNum);
+
+            if (operationSign != "/" && operationSign != "*" && operationSign != "+" && operationSign != "-")
+            {
+                Console.WriteLine("Invalid  arithmetic operation sign!");
+            }
+
+            else if ((firstNum > 0 || firstNum < 0) && (secondNum >= 0 || secondNum < 0) ) {
+                if(operationSign == "+")
+                {
+                    result = firstNum + secondNum;
+                }
+                if (operationSign == "-")
+                {
+                    result = firstNum - secondNum;
+                }
+                if (operationSign == "*")
+                {
+                    result = firstNum * secondNum;
+                }
+                if (operationSign == "/")
+                {
+                    result = firstNum / secondNum;
+                }
+                Console.WriteLine("Result: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Please input numbers or first number that is higher than 0!");
+                Console.ReadLine();
+            }
+
+            
+            Console.ReadLine();
+
+        }
+    }
+}
--- RealCalculator/Properties/AssemblyInfo.cs
+++ RealCalculator/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("RealCalculator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("RealCalculator")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("485e1110-5b48-4733-a768-71ac6dba1784")]
+[assembly: Guid("e6667111-f169-4b6a-a08e-65666bdcec01")]
 
 // Version information for an assembly consists of the following four values:
 //
