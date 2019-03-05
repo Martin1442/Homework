e14a775a0fdaba89df7580d18c3311b56eda5a58
Martin1442 <Martin1442@users.noreply.github.com>
2019-03-05 15:38:10 +0100

    RealCalculator

--- RealCalculator/Program.cs
+++ RealCalculator/Program.cs
@@ -13,56 +13,112 @@ namespace RealCalculator
             Console.WriteLine("To start the calculator press Enter");
             Console.ReadLine();
 
-            Console.WriteLine("Please enter your first number:");
-            string firstNumber = Console.ReadLine();
-
-            Console.WriteLine("Please enter your arithmetic operation sign e.g(+,-,*,/)");
-            string operationSign = Console.ReadLine();
-
-            Console.WriteLine("Please enter your second number:");
-            string secondNumber = Console.ReadLine();
-
-            int firstNum;
-            int secondNum;
-
-            int result = 0;
-
-            bool convertFirstNum = int.TryParse(firstNumber, out firstNum);
-            bool convertSecondNum = int.TryParse(secondNumber, out secondNum);
-
-            if (operationSign != "/" && operationSign != "*" && operationSign != "+" && operationSign != "-")
-            {
-                Console.WriteLine("Invalid  arithmetic operation sign!");
-            }
-
-            else if ((firstNum > 0 || firstNum < 0) && (secondNum >= 0 || secondNum < 0) ) {
-                if(operationSign == "+")
-                {
-                    result = firstNum + secondNum;
-                }
-                else if (operationSign == "-")
-                {
-                    result = firstNum - secondNum;
-                }
-                else if (operationSign == "*")
-                {
-                    result = firstNum * secondNum;
-                }
-                else if (operationSign == "/")
-                {
-                    result = firstNum / secondNum;
-                }
-                Console.WriteLine("Result: " + result);
-            }
-            else
-            {
-                Console.WriteLine("Please input numbers or first number that is higher than 0!");
-                Console.ReadLine();
-            }
-
+            string operationSign;
+            string firstNumber;
+            string secondNumber;
+            double firstNum;
+            double secondNum;
+            double result = 0;
+            int counter = 0;
+            var history = new string[0];
             
-            Console.ReadLine();
+            
 
+            while (true)
+            {
+                #region Operation
+                do
+                {
+                    Console.WriteLine("Please enter your arithmetic operation sign e.g(+,-,*,/)");
+                    operationSign = Console.ReadLine();
+                    if (operationSign != "/" && operationSign != "*" && operationSign != "+" && operationSign != "-")
+                    {
+                        Console.WriteLine("Invalid  arithmetic operation sign!");
+                        continue;
+                    }
+                    Array.Resize(ref history, history.Length + 1);
+                    history[counter] = operationSign;
+                    counter++;
+                    break;
+                } while (true);
+                #endregion
+                #region FirstNumber
+                do
+                {
+                    Console.WriteLine("Please enter your first number:");
+                    firstNumber = Console.ReadLine();
+
+                    bool convertFirstNum = double.TryParse(firstNumber, out firstNum);
+                    if (firstNum == 0)
+                    {
+                        Console.WriteLine("Invalid number");
+                        continue;
+                    }
+                    Array.Resize(ref history, history.Length + 1);
+                    history[counter] = firstNumber;
+                    counter++;
+                    break;
+                } while (true);
+                #endregion
+                #region SecondNumber
+                do
+                {
+                    Console.WriteLine("Please enter your second number:");
+                    secondNumber = Console.ReadLine();
+                    if (secondNumber == "0")
+                    {
+                        secondNum = 0;
+                        break;
+                    }
+                    bool convertSecondNum = double.TryParse(secondNumber, out secondNum);
+                    if (secondNum == 0)
+                    {
+                        Console.WriteLine("Invalid number");
+                        continue;
+                    }
+                    Array.Resize(ref history, history.Length + 1);
+                    history[counter] = secondNumber;
+                    counter++;
+                    break;
+                }
+                while (true);
+                #endregion
+                #region Calculation
+                if ((firstNum > 0 || firstNum < 0) && (secondNum >= 0 || secondNum < 0))
+                {
+                    if (operationSign == "+")
+                    {
+                        result = firstNum + secondNum;
+                    }
+                    else if (operationSign == "-")
+                    {
+                        result = firstNum - secondNum;
+                    }
+                    else if (operationSign == "*")
+                    {
+                        result = firstNum * secondNum;
+                    }
+                    else if (operationSign == "/")
+                    {
+                        result = firstNum / secondNum;
+                    }
+                    Console.WriteLine("Result: " + result);
+                }
+                #endregion
+                Console.WriteLine("If you want to continue type anything , or if you want to quit type:N");
+                string answer = Console.ReadLine();
+                if (answer == "N")
+                {
+                    for (var i = 0; i < history.Length; i++)
+                    {
+                        
+                        Console.WriteLine("History: operation " + history[i] + " with " + history[++i] + " , " + history[++i]);
+                    }
+                    
+                    Console.ReadLine();
+                    break;
+                }
+            }
         }
     }
 }
